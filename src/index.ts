285d8daeb71f7d4926c2041e773e1128f4538084
bsnk-dev <admin@bsnk.dev>
2021-07-03 20:03:58 -0500

    fixed exporting issue for ts/js interop

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import GraphQL from "./services/GraphQL";
 import api from './api';
 
+const setKey = (key: string) => {
+  GraphQL.setKey(key);
+};
+
+exports.setKey = setKey;
+
+for (const [key, value] of Object.entries(api)) {
+  exports[key] = value;
+}
+
 export default {
-  /**
-   * Set the API key to use with PnWKit
-   * @param {string} key P&W API key 
-   */
+ /**
+ * Set the API key to use with PnWKit
+ * @param {string} key P&W API key 
+ */
   setKey: (key: string) => {
-    GraphQL.setKey(key);
+  GraphQL.setKey(key);
   },
   ...api,
 }
