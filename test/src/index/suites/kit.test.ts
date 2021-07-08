12e10c8336024f8fb6e0aaf864ec511b75cbf76a
bsnk-dev <admin@bsnk.dev>
2021-07-08 17:11:39 -0500

    caching

--- test/src/index/suites/kit.test.ts
+++ test/src/index/suites/kit.test.ts
@@ -1,7 +1,12 @@
 import assert from 'assert';
+import {Suite} from 'mocha';
+import pnwkit from '../../../../src';
+
+// eslint-disable-next-line
+const memProfile = require('memoizee/profile');
 
 export default function suite() {
-  describe('Kit()', function(this: any) {
+  describe('Kit()', function(this: Suite) {
     it('should be able to retrieve a nation', async function() {
       const nations = await this.pnwkitFromClass.nationQuery(
           {first: 1, id: [100541]}, `
@@ -9,6 +14,27 @@ export default function suite() {
       `);
 
       assert.strictEqual(nations[0].nation_name, 'Blusania');
+    });
+
+    it('should be able to cache a call', async function() {
+      const cachableNationCall = pnwkit.cached(
+          pnwkit.nationQuery,
+          0.5,
+      );
+
+      await cachableNationCall(
+          {first: 1, id: [100541]}, 'nation_name');
+
+      await new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          await cachableNationCall(
+              {first: 1, id: [100541]}, 'nation_name');
+
+          resolve(undefined);
+        }, 2500);
+      });
+
+      assert.strictEqual(memProfile.statistics[Object.keys(memProfile.statistics)[0]].cached, 1);
     });
   });
 }
