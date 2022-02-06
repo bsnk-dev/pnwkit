9f68a904af75fc038437456d0d202b246811f349
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2022-02-05 22:22:02 -0600

    added bounty test

--- test/src/api/index.ts
+++ test/src/api/index.ts
@@ -5,6 +5,7 @@ import WarQuery from './suites/WarQuery.test';
 import TradeQuery from './suites/TradeQuery.test';
 import TreasureQuery from './suites/TreasureQuery.test';
 import ColorQuery from './suites/ColorQuery.test';
+import BountyQuery from './suites/BountyQuery.test';
 
 describe('api', function() {
   NationQuery.bind(this)();
@@ -14,4 +15,5 @@ describe('api', function() {
   WarQuery.bind(this)();
   TreasureQuery.bind(this)();
   ColorQuery.bind(this)();
+  BountyQuery.bind(this)();
 });
